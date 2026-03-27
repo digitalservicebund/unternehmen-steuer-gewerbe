4115f32118cdf61678c9c76fe1f64844e29290c4
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2026-03-27 15:02:27 +0100

    Expect 404 as number

--- src/app.test.ts
+++ src/app.test.ts
@@ -8,6 +8,6 @@ let request = supertest(app);
 describe("API tests", () => {
   test("Returns 404 NOT FOUND", async () => {
     const response = await request.get("/unsupported-url");
-    expect(response.statusCode).toEqual("404");
+    expect(response.statusCode).toEqual(404);
   });
 });
