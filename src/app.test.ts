8e20750aba0465abd8bb6c3037bccd3541daae39
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2026-03-28 21:12:57 +0100

    Return 200 on /register-steuernummer (red test)

--- src/app.test.ts
+++ src/app.test.ts
@@ -6,8 +6,13 @@ import app from "./app.js";
 let request = supertest(app);
 
 describe("API tests", () => {
-  test("Returns 404 NOT FOUND", async () => {
+  test("Returns 404 NOT FOUND on unsupported route", async () => {
     const response = await request.get("/unsupported-url").send();
     expect(response.statusCode).toEqual(404);
+  });
+
+  test("Returns 200 OK on POST to /register-steuernummer", async () => {
+    const response = await request.post("/register-steuernummer").send();
+    expect(response.statusCode).toEqual(200);
   });
 });
