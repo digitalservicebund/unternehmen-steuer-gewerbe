64decd94e6ae5aeb60441a3a9981acfc100efe1d
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2026-03-28 21:06:01 +0100

    Ignore coverage for now

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { type Express } from "express";
 import helmet from "helmet";
 
-/* v8 ignore next 2 */
 const app: Express = express();
 app.use(helmet.hidePoweredBy());
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import app from "./app.js";
 
-/* v8 ignore next 3*/
 app.listen(3000, () => {
   console.log("Express app listening on port 3000");
 });
